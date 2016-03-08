ef570c9bcaa0bfd152238cdac870f813ad16fe33
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2016-03-07 22:19:50 -0800

    calling adb devices.

--- adb-viewer/adb-viewer/ViewController.swift
+++ adb-viewer/adb-viewer/ViewController.swift
@@ -10,18 +10,28 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
+		let task: NSTask = NSTask()
+		let pipe: NSPipe = NSPipe()
+
+		task.launchPath = "/usr/local/bin/adb"
+		task.arguments = ["devices"]
+		task.standardOutput = pipe
+		task.launch()
+
+		let handle = pipe.fileHandleForReading
+		let data = handle.readDataToEndOfFile()
+        let result_s = NSString(data: data, encoding: NSUTF8StringEncoding)
+        result_s?.capitalizedString
         // Do any additional setup after loading the view.
-    }
+	}
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
+	override var representedObject: AnyObject? {
+		didSet {
+			// Update the view, if already loaded.
+		}
+	}
 }
 
